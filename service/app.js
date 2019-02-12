6dc393f5155ce3c1abb75664a4ab81ae0aa692ec
RaistlinTAO <DemonVK@Gmail.com>
2019-02-13 06:04:29 +1100

    MySQL integration

--- service/app.js
+++ service/app.js
@@ -1,3 +1,6 @@
+/**
+ * @EXPRESS
+ */
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -5,6 +8,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+/**
+ * @MYSQL
+ */
+var mysql = require('mysql');
 
 var app = express();
 
@@ -15,8 +23,41 @@ var app = express();
 
 
  ***************************/
+
+//MYSQL TABLE
+/**
+* @PARKING_DATABASE.SQL
+**/
+var con = mysql.createConnection({
+    host: "203.87.7.66",
+    port: "3306",
+    user: "twoshot",
+    password: "7ujm8ik,",
+    database: "parking"
+});
+
 app.get('/test', function (req, res) {
     res.send('Hello World!');
+});
+
+app.post("/getparkingspaces", function (request, response) {
+
+    var lat = request.body.lat;
+    var lng = request.body.lng;
+    //The location data is user's current GPS location
+    //The server than send back spaces within a lat/lng range.
+    console.log('Prepared data');
+    con.query('SELECT * from space WHERE spaceLat = \"' +  ""  + '\"', function (err, rows, fields) {
+        if (!err) {
+            console.log(rows);
+            response.send(rows);
+        }
+        else {
+            //ERROR
+            response.send('ERROR');
+        }
+
+    });
 });
 
 app.use(logger('dev'));
