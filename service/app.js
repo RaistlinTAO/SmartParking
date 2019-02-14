e14136bb75b0da01ffdc986f6269754ce38065ab
RaistlinTAO <DemonVK@Gmail.com>
2019-02-14 15:48:40 +1100

    Update the /getparkingspaces entry point so the function accept the offset parameter to manipulate the area

--- service/app.js
+++ service/app.js
@@ -27,17 +27,17 @@ var app = express();
 
 //MYSQL TABLE
 /**
-* @PARKING_DATABASE.SQL
-**/
+ * @PARKING_DATABASE.SQL
+ **/
     //PARKING_DATABASE.SQL used for creating the table, the data source is:
     //https://data.melbourne.vic.gov.au/Transport-Movement/On-street-Parking-Bay-Sensors/vh2v-4nfs
 var con = mysql.createConnection({
-    host: "manyi.ga", //change to alt server
-    port: "3306",
-    user: "twoshot",
-    password: "7ujm8ik,",
-    database: "parking"
-});
+        host: "manyi.ga", //change to alt server
+        port: "3306",
+        user: "twoshot",
+        password: "7ujm8ik,",
+        database: "parking"
+    });
 
 app.get('/test', function (req, res) {
     res.send('Service Running');
@@ -47,25 +47,55 @@ app.post("/getparkingspaces", function (request, response) {
 
     var lat = request.body.lat;
     var lng = request.body.lng;
-
+    var offset = request.body.offset;
+    var decimaldegree = 0.00000001;
     //The location data is user's current GPS location
     //The server than send back spaces within a lat/lng range.
     console.log('Prepared data');
     //TODO: The database should fetch realtime data from other parties when at the final stage.
     //TODO: However, in this prototype we return real data but use random status (occupied) to demonstrate.
 
-    //In the final stage, the SQL statement should be like this:
-    //WHERE `spaceLat` >= 40.225619  (lat - 0.000010)
-    //   AND `spaceLat` <= 40.226561  (lat + 0.000010)
-    //   AND `spaceLng` >= -74.223882   (lng - 0.000010)
-    //   AND `spaceLng` <= -74.220861;  (lng + 0.000010)
-    con.query('SELECT * from space WHERE spaceLat >= ' +  lat - 0.001  + ' AND spaceLat<='+  lat + 0.001  +
-        ' AND spaceLng <= ' +  lng - 0.001  + ' AND spaceLng<='+  lng + 0.001, function (err, rows, fields) {
+    // A value in decimal degrees to a precision of 4 decimal places is precise to 11.132 meters at the equator. A value
+    // in decimal degrees to 5 decimal places is precise to 1.1132 meter at the equator.
+    switch (offset) {
+        case 0:
+            decimaldegree = 1; //country or large region  +-111.32 km
+            break;
+        case 1:
+            decimaldegree = 0.1; //large city or district   +-11.132 km
+            break;
+        case 2:
+            decimaldegree = 0.01; //town or village +-1.1132 km
+            break;
+        case 3:
+            decimaldegree = 0.001; //neighborhood, street +-111.32 m
+            break;
+        /////////////////////////////////////////////////////////////////////////
+            //WE DONT REALLY NEED THESE BELOW
+        /////////////////////////////////////////////////////////////////////////
+        case 4:
+            decimaldegree = 0.0001; //individual street, land parcel +-	11.132 m
+            break;
+        case 5:
+            decimaldegree = 0.00001; //individual trees, door entrance +-1.1132 m
+            break;
+        case 6:
+            decimaldegree = 0.000001; //individual humans +-111.32 mm
+            break;
+        case 7:
+            decimaldegree = 0.0000001; //practical limit of commercial surveying +-11.132 mm
+            break;
+        case 8:
+            decimaldegree = 0.00000001; //specialized surveying (e.g. tectonic plate mapping) +-1.1132 mm
+            break;
+    }
+
+    con.query('SELECT * from space WHERE spaceLat >= ' + lat - decimaldegree + ' AND spaceLat<=' + lat + decimaldegree +
+        ' AND spaceLng <= ' + lng - decimaldegree + ' AND spaceLng<=' + lng + decimaldegree, function (err, rows, fields) {
         if (!err) {
             //console.log(rows);
             response.send(rows);
-        }
-        else {
+        } else {
             //ERROR
             response.send('ERROR');
         }
@@ -75,7 +105,7 @@ app.post("/getparkingspaces", function (request, response) {
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
